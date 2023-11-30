33c9157265c1c10dace247d353ff39a626b3b8bb
Areg <areg.ayvazyan5@gmail.com>
2023-11-30 19:36:28 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from typing import Union
+from fastapi import FastAPI
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = "people"
+
+    ssn = Column("ssn", Integer, primary_key=True)
+    firstname = Column("firstname", String)
+    lastname = Column("lastname", String)
+    gender = Column("gender", CHAR)
+    age = Column("age", Integer)
+
+    def __init__(self, ssn, first, last, gender, age):
+        self.ssn = ssn
+        self.firstname = first
+        self.lastname = last
+        self.gender = gender
+        self.age = age
+
+    def __repr__(self):
+        return f"({self.ssn}) {self.firstname} {self.lastname} ({self.gender}, {self.age})"
+
+
+engin = create_engine("sqlite:///mydb.db, echo=True")
+Base.metadata.create_all(bind=engin)
+
+Session = sessionmaker(bind=engin)
+session = Session()
+
+person = Person(2, "Mike", "Smith", "m", 35)
+session.add(person)
+session.commit()
+
+
+
+
+
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+#
+#
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+#
